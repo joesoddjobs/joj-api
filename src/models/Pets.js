6dcf69aae3226c792eba6b3a4f0377a01f02218b
Marwa <marwalbaadani@gmail.com>
2019-05-05 17:07:13 -0400

    add pet/human relation

--- src/models/Pets.js
+++ src/models/Pets.js
@@ -2,5 +2,18 @@ class Pets extends BaseModel {
   static get tableName() {
     return 'pets'
   }
+  static get relationMappings() {
+    return {
+      educations: {
+        relation: HasManyRelation,
+        modelClass: User,
+        join: {
+          from: 'users.id',
+          to: 'pets.Id',
+        },
+      },
+    }
+  }
 }
+
 module.exports = Pets
