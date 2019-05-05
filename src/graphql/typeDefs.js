6180a2f2e7c593b090f25a92bdbd92b1c588048d
joviharvard <43095870+joviharvard@users.noreply.github.com>
2019-05-05 17:31:45 -0400

    Revert "Types"

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,36 +2,6 @@ const gql = require('graphql-tag')
 
 module.exports = gql`
   type Query {
-    pets(id: ID!): [Pet!]!
-  }
-
-  type Mutation {
-
-  }
-
-  type User {
-    id: ID!
-    email: String!
-    name: String! 
-    pets: PetsTypes
-  }
-
-  type Pet {
-    userId: ID!
-    color: ColorTypes
-  }
-
-  enum ColorTypes {
-    ORANGE
-    BLACK 
-    WHITE 
-    STRIPED
-  }
-
-  enum PetsTypes {
-    CAT
-    BIG CAT
-    ANOTHER CAT
-    SMALL CAT
+    welcome: Int
   }
 `
